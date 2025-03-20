89d3bd21bbe637cb1530059b9e4879bba922eb47
unknown <hanijarrah20 git config --global user.email hanijarrah20>
2025-03-20 21:50:34 +0300

    Task6

--- Task 5/Tasks/Tasks.Server/obj/Debug/net8.0/Tasks.Server.AssemblyInfo.cs
+++ Task 5/Tasks/Tasks.Server/obj/Debug/net8.0/Tasks.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tasks.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e14f19bfa1fbf8f5d2fb31a42249ee9f4758c5a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tasks.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tasks.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
